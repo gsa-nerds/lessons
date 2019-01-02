5a1ab70640aec3bc0bfafb1852f06418e7ab0534
Shy Shalom <shooshx@gmail.com>
2019-01-02 15:13:29 +0200

    added param to kernel module

--- linux_kmoudle/kmain.c
+++ linux_kmoudle/kmain.c
@@ -2,23 +2,55 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 
-//int* g_ptr = NULL;
 
+// ------------ simple parameter definition
+static unsigned int some_param = 1;
+module_param(some_param, uint, 0644);
+MODULE_PARM_DESC(some_param, "This is some parameter");
 
-static int inf_rec(void) {
-    return inf_rec();
+// ------------ more complex (flexible) parameter definition
+
+static unsigned int trigger_param = 2;
+
+static int myparam_set(const char* buffer, const struct kernel_param* kp)
+{
+    // this is a toy example, a real thing would do atoi()
+    trigger_param = buffer[0];
+    return 0;  
 }
+static int myparam_get(char* val, const struct kernel_param* kp) 
+{
+    // this is a toy example, a real thing would do sprintf(val, "%d", x) or itoa()
+    val[0] = 'X';
+    val[1] = 'Y';
+    val[2] = '\n';
+    val[3] = 0;
+    return 3;
+}
+
+
+static const struct kernel_param_ops myops = {
+  .set = myparam_set,
+  .get = myparam_get
+};
+
+module_param_cb(trigger_param, &myops, &trigger_param, 0644);
+
+
+// ---------------- initialization and destruction of the module
 
 static int my_init(void) {
     printk("Hello Kernel!\n");
-    //*g_ptr = 1;
-    inf_rec();
     return 0;
 }
 
+// exit is mandatory, otherwise the module can't go down
 static void my_exit(void) {
-    printk("kmain exiting\n");
+    printk("kmain exiting %d trigger=%d\n", some_param, trigger_param);
 }
 
 module_init(my_init);
 module_exit(my_exit);
+
+
+
--- linux_kmoudle/readme.txt
+++ linux_kmoudle/readme.txt
@@ -8,3 +8,20 @@ add the Makefile the command that runs the kernel build system Makefile, which l
 sudo dmesg -c   - show kernel log AND clear it
 
 sudo insmod kmain.ko    - load the module to the kernel
+sudo rmmod kmain   -remove the module
+sudo modinfo kmain  -show info about loaded module
+lsmod  - show .kos that are currently loaded
+sudo modprobe  - higher level control to add/remove modules
+
+
+develpemt cycle:
+- change code
+- make all  in code directory
+- sudo rmmod kmain   - remove the old loaded module
+    - /sys/module/kmain/parameters  - will be deleted, if you are in there, there will be a deleted folder you are in, 
+    - cd ../..  to get to /sys/module
+    - cd kmain/parameters to get to the newly created folder for the newly loaded module
+- sudo insmod kmain  - load the newly compiled
+- cd /sys/module/kmain/parameters  - parameters directory for module
+- sudo chmod a+w param_name - make it writable to normal user (not root)
+
